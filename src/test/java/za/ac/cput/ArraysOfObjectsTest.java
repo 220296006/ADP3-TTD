f2ed5f8b961681761937266faea1a4321bad6e1f
Thabiso <220296006@mycput.ac.za>
2022-02-10 01:15:13 +0200

    Test for Object Equality passed

--- src/test/java/za/ac/cput/ArraysOfObjectsTest.java
+++ src/test/java/za/ac/cput/ArraysOfObjectsTest.java
@@ -4,21 +4,25 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.Assert.*;
 
 public class ArraysOfObjectsTest {
 
-    private Student student;
-
-    @Before
-    public void setUp() throws Exception {
-    }
-
-    @After
-    public void tearDown() throws Exception {
-    }
-
+    /** Testing for Object Equality
+     */
     @Test
-    public void main() {
+    public void test() {
+        List<Student> studentList = new ArrayList<>();
+
+        for (int i = 0; i < 4; i++) {
+            Student student = new Student("Thabiso", 99);
+            studentList.add(student);
+
+        }
+        assertEquals(4, studentList.size());
+
     }
 }
