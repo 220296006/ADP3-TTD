3c5b3bbbc2eb4340e4b2f174c5696d2cc0964a2d
Thabiso <220296006@mycput.ac.za>
2022-02-10 02:19:02 +0200

    Test for Object Identity passed

--- src/test/java/za/ac/cput/ArraysOfObjectsTest.java
+++ src/test/java/za/ac/cput/ArraysOfObjectsTest.java
@@ -11,7 +11,7 @@ import static org.junit.Assert.*;
 
 public class ArraysOfObjectsTest {
 
-    /** Testing for Object Equality
+    /** Testing for Object Identity
      */
     @Test
     public void test() {
@@ -22,7 +22,7 @@ public class ArraysOfObjectsTest {
             studentList.add(student);
 
         }
-        assertEquals(4, studentList.size());
+        assertSame( 4, studentList.size());
 
     }
 }
