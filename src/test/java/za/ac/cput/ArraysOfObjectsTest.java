ada8f6ce24968bb73178bb478e00a9f3197b954a
Thabiso <220296006@mycput.ac.za>
2022-02-10 04:21:01 +0200

    Test for Timeouts

--- src/test/java/za/ac/cput/ArraysOfObjectsTest.java
+++ src/test/java/za/ac/cput/ArraysOfObjectsTest.java
@@ -6,23 +6,26 @@ import org.junit.Test;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 import static org.junit.Assert.*;
 
 public class ArraysOfObjectsTest {
 
-    /** Testing for FailTest
+    /** Testing for Timeouts
      */
-    @Test
-    public void test() {
+    @Test(timeout = 500)
+    public void test() throws InterruptedException{
         List<Student> studentList = new ArrayList<>();
+
+        TimeUnit.SECONDS.sleep(100);
 
         for (int i = 0; i < 4; i++) {
             Student student = new Student("Thabiso", 99);
             studentList.add(student);
 
         }
-        fail();
+
 
     }
 }
