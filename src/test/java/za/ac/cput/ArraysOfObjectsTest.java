9eb0a8a6b1f173e9af43a22479c6fc24fb901d55
Thabiso <220296006@mycput.ac.za>
2022-02-10 04:27:44 +0200

    Test for Disabling Test

--- src/test/java/za/ac/cput/ArraysOfObjectsTest.java
+++ src/test/java/za/ac/cput/ArraysOfObjectsTest.java
@@ -2,6 +2,7 @@ package za.ac.cput;
 
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 
 import java.util.ArrayList;
@@ -12,14 +13,12 @@ import static org.junit.Assert.*;
 
 public class ArraysOfObjectsTest {
 
-    /** Testing for Timeouts
+    /** Testing for Disabling
      */
-    @Test(timeout = 500)
-    public void test() throws InterruptedException{
+    @Ignore
+    @Test
+    public void test() {
         List<Student> studentList = new ArrayList<>();
-
-        TimeUnit.SECONDS.sleep(100);
-
         for (int i = 0; i < 4; i++) {
             Student student = new Student("Thabiso", 99);
             studentList.add(student);
